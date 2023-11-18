388d14ac23bd83687acac58aaf85ea96db885aac
tiagoalmeida25 <tiago.ta.almeida25@gmail.com>
2023-11-18 19:01:08 +0000

    design for main page, bottom nav

---
+++ lib/components/custom_nav_bar.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class CustomNavBar extends StatelessWidget {
+  final Color buttonColor;
+  final Function() onPressed;
+
+  const CustomNavBar(
+      {Key? key, required this.buttonColor, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: SizedBox(
+            width: size.width,
+            height: 90,
+            child: Stack(
+              children: [
+                CustomPaint(
+                  size: Size(size.width, 90),
+                  painter: BNBCustomPainter(),
+                ),
+                Center(
+                  heightFactor: 1.1,
+                  child: FloatingActionButton(
+                    backgroundColor: buttonColor,
+                    elevation: 0.1,
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.add,
+                      color: Color.fromRGBO(37, 42, 48, 1),
+                    ),
+                  ),
+                ),
+                Container(
+                    width: size.width,
+                    height: 90,
+                    padding:
+                        const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                            onPressed: onPressed,
+                            icon: const Icon(Icons.home_filled,
+                                color: Color.fromRGBO(186, 186, 186, 1),
+                                size: 40)),
+                        IconButton(
+                            onPressed: onPressed,
+                            icon: const Icon(Icons.history,
+                                color: Color.fromRGBO(186, 186, 186, 1),
+                                size: 40)),
+                        Container(width: size.width * 0.20),
+                        IconButton(
+                            onPressed: onPressed,
+                            icon: const Icon(Icons.bar_chart,
+                                color: Color.fromRGBO(186, 186, 186, 1),
+                                size: 40)),
+                        IconButton(
+                            onPressed: onPressed,
+                            icon: const Icon(Icons.person,
+                                color: Color.fromRGBO(186, 186, 186, 1),
+                                size: 40))
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BNBCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = const Color.fromRGBO(37, 42, 48, 1)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path()..moveTo(42, 16);
+
+    path.lineTo(size.width * 0.40, 16);
+    path.arcToPoint(Offset(size.width * 0.60, 16),
+        radius: const Radius.circular(45.0), clockwise: true);
+    path.lineTo(size.width - 42, 16);
+    path.quadraticBezierTo(size.width - 16, 16, size.width - 16, 42);
+    path.lineTo(size.width - 16, size.height - 42);
+    path.quadraticBezierTo(
+        size.width - 16, size.height - 16, size.width - 42, size.height - 16);
+    path.lineTo(42, size.height - 16);
+    path.quadraticBezierTo(16, size.height - 16, 16, size.height - 42);
+    path.lineTo(16, 42);
+    path.quadraticBezierTo(16, 16, 42, 16);
+    path.close();
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+
 import 'package:trackerapp/app_colors.dart';
 import 'package:trackerapp/bloc/login_bloc.dart';
+import 'package:trackerapp/components/custom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -37,91 +40,242 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minWidth: MediaQuery.of(context).size.width,
-                minHeight: MediaQuery.of(context).size.height,
-              ),
-              child: IntrinsicHeight(
-                  child:
-                      Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 30,
-                      offset: const Offset(2, 0),
-                    ),
-                  ]),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(32),
-                        bottomRight: Radius.circular(32)),
-                    child: Container(
-                      height: 110,
-                      padding: const EdgeInsets.only(left: 20, top: 35),
-                      color: const Color.fromRGBO(37, 42, 48, 1),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Welcome, ',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(username,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: MediaQuery.of(context).size.width,
+                  minHeight: MediaQuery.of(context).size.height,
+                ),
+                child:
+                    Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 30,
+                        offset: const Offset(2, 0),
+                      ),
+                    ]),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(32),
+                          bottomRight: Radius.circular(32)),
+                      child: Container(
+                        height: 110,
+                        padding: const EdgeInsets.only(left: 20, top: 35),
+                        color: const Color.fromRGBO(37, 42, 48, 1),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Welcome, ',
                               style: TextStyle(
-                                  color: primary[10],
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold)),
-                        ],
+                                color: Colors.white,
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(username,
+                                style: TextStyle(
+                                    color: primary[10],
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.bold)),
+                            const Text('!',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 36,
+                                  fontWeight: FontWeight.bold,
+                                ))
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Column(children: <Widget>[
-                      Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(16),
-                            child: Container(
-                              color: const Color.fromRGBO(37, 42, 48, 1),
-                              padding: const EdgeInsets.all(8),
-                              height: 30,
-                              width: 70,
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: Container(
+                                    color: const Color.fromRGBO(37, 42, 48, 1),
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8, bottom: 6),
+                                    width: 70,
+                                    child: const Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                            height: 16,
+                                            child: Icon(Icons.list_rounded,
+                                                color: Color.fromRGBO(
+                                                    186, 186, 186, 1),
+                                                size: 24)),
+                                        SizedBox(width: 4),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 4.0),
+                                          child: Text('All',
+                                              style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      186, 186, 186, 1),
+                                                  fontSize: 16)),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Row(children: [
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Container(
+                                        height: 30,
+                                        width: 30,
+                                        color:
+                                            const Color.fromRGBO(37, 42, 48, 1),
+                                      )),
+                                  const SizedBox(width: 8),
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Container(
+                                        height: 30,
+                                        width: 30,
+                                        color:
+                                            const Color.fromRGBO(37, 42, 48, 1),
+                                      )),
+                                  const SizedBox(width: 8),
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Container(
+                                        height: 30,
+                                        width: 30,
+                                        color:
+                                            const Color.fromRGBO(37, 42, 48, 1),
+                                      ))
+                                ]),
+                              ],
+                            ),
+                            const SizedBox(height: 16),
+                            Container(
+                              width: 150,
+                              padding: const EdgeInsets.only(
+                                  left: 8, right: 8, top: 4, bottom: 4),
+                              decoration: BoxDecoration(
+                                  color: const Color.fromRGBO(37, 42, 48, 1),
+                                  borderRadius: BorderRadius.circular(16)),
                               child: const Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  SizedBox(
-                                      height: 16,
-                                      width: 24,
-                                      child: Icon(Icons.list,
-                                          color:
-                                              Color.fromRGBO(186, 186, 186, 1),
-                                          size: 24)),
-                                  SizedBox(width: 4),
-                                  Text('All',
+                                  Icon(Icons.add,
+                                      color: Color.fromRGBO(186, 186, 186, 1),
+                                      size: 24),
+                                  Text('New Category',
                                       style: TextStyle(
                                           color:
                                               Color.fromRGBO(186, 186, 186, 1),
-                                          fontSize: 14)),
+                                          fontSize: 16)),
                                 ],
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ])),
-              ])))),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height - 300,
+                              child: ListView.builder(
+                                itemCount: 8,
+                                itemBuilder: (BuildContext context, int index) {
+                                  List<String> words = [
+                                    'Transport',
+                                    'Food',
+                                    'TV Shows',
+                                    'Movies',
+                                    'Games',
+                                    'Books',
+                                    'Music',
+                                    'Clothes'
+                                  ];
+                                  double fontSize = 20.0;
+                                  TextPainter textPainter = TextPainter(
+                                    text: TextSpan(
+                                      text: words[index],
+                                      style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+                                    ),
+                                    maxLines: 1,
+                                    textDirection: TextDirection.ltr,
+                                  );
+                                  textPainter.layout();
+                                  double wordWidth = textPainter.width;
+                                  double wordHeight = textPainter.height;
+
+                                  return Stack(
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 8),
+                                        child: Container(
+                                          height: wordHeight + 36,
+                                          decoration: BoxDecoration(
+                                            color: primary[index + 1],
+                                            borderRadius:
+                                                BorderRadius.circular(32),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.black
+                                                    .withOpacity(0.5),
+                                                spreadRadius: 5,
+                                                blurRadius: 15,
+                                                offset: const Offset(-1, 0),
+                                                inset: true,
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            bottom: 8,
+                                            left: 24,
+                                            right: 8,
+                                            top: 10),
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(32),
+                                          child: Container(
+                                            width: wordWidth + 24,
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 12, vertical: 8),
+                                            color:
+                                                Colors.black.withOpacity(0.3),
+                                            child: Text(
+                                              words[index],
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
+                          ])),
+                ])),
+          ),
+          CustomNavBar(
+            buttonColor: primary[10]!,
+            onPressed: () {},
+          )
+        ],
+      ),
     );
   }
 }
